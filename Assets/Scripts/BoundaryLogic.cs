fde44697832d54a27be951263969a61c82a3c297
Ritchie Hofmann <rhofma2@lsu.edu>
2016-03-02 15:36:32 -0600

    Saving work to pull

---
+++ Assets/Scripts/BoundaryLogic.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundaryLogic : MonoBehaviour {
+
+	void OnCollisionExit(Collision tetromino) {
+		Debug.Log ("Tetromino Leaving play area");
+		tetromino.collider.GetComponent<Tetromino> ().ReverseDirection ();
+	}
+}
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Tetromino : MonoBehaviour {
 	private Vector3 randomDirection;
+	//private Rigidbody rb;
 
 	public int shape;
 
@@ -11,10 +12,13 @@ public class Tetromino : MonoBehaviour {
 		randomDirection = new Vector3 (Random.Range(-1.0f, 1.0f), Random.Range (-1.0f, 1.0f), 0.0f);
 		transform.Rotate (randomDirection);
 	}
-	
-	// Update is called once per frame
+
 	void Update () {	
 		//randomDirection = new Vector3 (Random.value, Random.value, 0.0f);
 		transform.position += randomDirection * 2.0f * Time.deltaTime;
+	}
+
+	public void ReverseDirection() {
+		randomDirection = randomDirection * -1.0f;
 	}
 }
