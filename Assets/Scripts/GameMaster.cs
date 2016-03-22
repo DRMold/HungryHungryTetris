8041812d4a67dab4fe0be6c7647d22d5c49c8765
Mohammad Hasan Firdausi <mfirda1@lsu.edu>
2016-03-22 13:40:41 -0500

    Add initial concept for interactable menu buttons

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class GameMaster : MonoBehaviour {
 
+	private int numPlayers;
 	// Use this for initialization
 	void Start () {
-	
+		numPlayers = 4;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void setNumPlayers(int num) {
+		numPlayers = num;
+		Debug.Log ("Number of players: " + numPlayers);
 	}
 }
---
+++ Assets/Scripts/UI/OptionsButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+
+[AddComponentMenu("Scripts/UI/Options Button")]
+public class OptionsButton : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnEnable() 
+	{
+		GetComponent<PressGesture> ().Pressed += optionsHandler;
+	}
+	
+	private void OnDisable() 
+	{
+		GetComponent<PressGesture> ().Pressed -= optionsHandler;
+	}
+	
+	private void optionsHandler(object sender, System.EventArgs e) 
+	{
+		Debug.Log ("Options button pressed!");
+	}
+}
