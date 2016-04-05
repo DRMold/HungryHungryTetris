5911c26dfd8e9b8b87bffa4ab2a051423660e44b
Mohammad Hasan Firdausi <mfirda1@lsu.edu>
2016-04-04 20:03:12 -0500

    Fix interaction with game over buttons during game

--- Assets/Scripts/UI/InGameOverButton.cs
+++ Assets/Scripts/UI/InGameOverButton.cs
@@ -1,30 +1,41 @@
 using UnityEngine;
 using System.Collections;
 using TouchScript.Gestures;
+using UnityEngine.UI;
 
 [AddComponentMenu("Scripts/UI/InGame GameOver Button")]
 public class InGameOverButton : MonoBehaviour {
 
     private string buttonType;
+    private Button myButton;
 
-	void Start ()
+    void Awake()
     {
         buttonType = gameObject.name;
-	}
+        myButton = GetComponent<Button>();
+        myButton.enabled = false;
+        myButton.interactable = false;
+    }
 
     void OnEnable()
     {
-        GetComponent<PressGesture>().Pressed += pressHandler;
+        GameMaster.StartListening("ShowGameOver", overHandler);
     }
 
     void OnDisable()
     {
         GetComponent<PressGesture>().Pressed -= pressHandler;
+        GameMaster.StopListening("ShowGameOver", overHandler);
     }
 	
     private void pressHandler(object sender, System.EventArgs e)
     {
         Debug.Log("GameOver Screen button pressed");
+        if (!myButton.enabled)
+        {
+            Debug.LogError("Pressing a game over button when not supposed to be able to!");
+            return;
+        }
         if (buttonType.Equals("RestartButton"))
         {
             GameMaster.TriggerEvent("GameRestart");
@@ -37,5 +48,12 @@ public class InGameOverButton : MonoBehaviour {
         {
             Debug.LogError("Game Over Button Press: Invalid name!");
         }
+    }
+
+    private void overHandler()
+    {
+        myButton.enabled = true;
+        myButton.interactable = true;
+        GetComponent<PressGesture>().Pressed += pressHandler;
     }
 }
