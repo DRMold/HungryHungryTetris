c05b6738b551206e38b6f65261479b082963cfb3
Matthew Wolff <mwolff3@lsu.edu>
2016-04-21 00:27:03 -0500

    updated some comments

--- Assets/Scripts/GoalLogic.cs
+++ Assets/Scripts/GoalLogic.cs
@@ -7,15 +7,23 @@ public class GoalLogic : MonoBehaviour {
 	public GameObject opponentQueue;
 	
 	void OnTriggerEnter(Collider tetromino) {
-		//Have control flow based off type? Or shape will be property of tetromino?
+		// shape      -        int
+		//   s                  0
+		//   i                  1
+		//   o                  2
+		//   j                  3
+		//   t                  4
+		//   l                  5
+		//   z                  6
+		// ^^^ don't know if above is correct rn
+		//   FFS                7
+		//   SFS                8
 		int shape = tetromino.gameObject.GetComponent<Tetromino>().shape;
 		Debug.Log ("Got shape " + shape);
-		//shape number does not reflect numbers properly
 		if (shape < 7) {
 			playerQueue.GetComponent<PrevTetris> ().AddToQueue (shape);
 			Debug.Log ("GameObject Added to Queue: " + shape);
 		} else {
-			//temporary select powerups, will be changed to link with powerup texture / model
 			int power = shape-7;
 			Debug.Log ("Applying powerup #" + power);
 			switch (power) {
@@ -23,12 +31,12 @@ public class GoalLogic : MonoBehaviour {
 				//Increase Fall speed
 				opponentQueue.GetComponent<PrevTetris>().speedUp();
 				break;
-			case 2:
-				//A bunch of blocks created on player's tetris playfield
-				break;
 			case 1:
 				//reduce fall speed, gets reset by method
 				playerQueue.GetComponent<PrevTetris>().slowDown();
+				break;
+			case 2:
+				//A bunch of blocks created on player's tetris playfield
 				break;
 			case 3:
 				//piece cycle
